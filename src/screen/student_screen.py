3f071e1d2d587a8ad0ca63514143ac700b25153c
Aniruddha090989 <aniruddha75@outlook.com>
2026-06-01 03:03:13 +0530

    Update student_screen.py

--- src/screen/student_screen.py
+++ src/screen/student_screen.py
@@ -8,14 +8,14 @@ from src.pipelines.face_pipeline import predict_attendance, get_face_embeddings,
 from src.pipelines.voice_pipeline import get_voice_embedding
 from src.database.db import (
     get_all_students, 
-    create_student, 
+    create_student_complete,  
     get_student_subjects, 
     get_student_attendance,
     unenroll_student_to_subject, 
-    check_student_username_exists,
-    student_login_username_password,
+    check_student_username_exists,  
+    student_login_username_password,  
     get_student_by_voice,
-    create_student_complete
+    get_student_by_id  
 )
 import time
 from src.components.dialog_enroll import enroll_dialog
